db833c2adc610dfbaf4fa905c10b34865d8680e6
runner <runner@Mac-1726605044630.local>
2024-09-17 20:59:36 +0000

    Update BitcoinDevKit.swift and Package.swift for release 1.0.0-beta2

--- Package.swift
+++ Package.swift
@@ -22,21 +22,15 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .binaryTarget(
-//            name: "bdkFFI",
-//            url: "https://github.com/bitcoindevkit/bdk-swift/releases/download/0.3.0/bdkFFI.xcframework.zip",
-//            checksum: "7d4a2fdeb03fb3eff107e45ee3148dd9b67966406c82d6e3c19f653c27180cfd"),
-        .binaryTarget(name: "bdkFFI", path: "./bdkFFI.xcframework"),
+        .binaryTarget(
+            name: "bdkFFI",
+            url: "https://github.com/nieaowei/bdk-swift/releases/download/1.0.0-beta2/bdkFFI.xcframework.zip",
+            checksum: "acfd7489ef41ecc489d2b2bdbc39ecf7e229e8a0e750c8064feb12950e744585"),
         .target(
             name: "BitcoinDevKit",
-            dependencies: ["bdkFFI"]
-        ),
+            dependencies: ["bdkFFI"]),
         .testTarget(
             name: "BitcoinDevKitTests",
-            dependencies: ["BitcoinDevKit"],
-            resources: [
-                .copy("Resources/pre_existing_wallet_persistence_test.sqlite")
-            ]
-        ),
+            dependencies: ["BitcoinDevKit"]),
     ]
 )
